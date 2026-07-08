f5a4c0558cd743945f705c3847dfd99832c97db5
Tim Cayanga <62490515+timcayanga@users.noreply.github.com>
2026-07-08 18:53:34 +0800

    Fix splash clover leaf layout

--- web/components/clover-loading-mark.tsx
+++ web/components/clover-loading-mark.tsx
@@ -2,11 +2,11 @@ const markPaths = {
   topLeft:
     "M0 40C0 28.9543 8.9543 20 20 20C20 8.9543 28.9543 0 40 0C51.0457 0 60 8.9543 60 20V60H20C8.95431 60 0 51.0457 0 40Z",
   topRight:
-    "M60 40C60 51.0457 48.8071 60 35 60C30.5563 60 26.3837 59.0716 22.7676 57.4453L20.0713 60.1426L11.4639 51.5352L24 39L21.1719 36.1719L8.63574 48.707L0 40.0713L2.6416 37.4287C0.960359 33.7678 0 29.524 0 25C0 11.1929 8.95431 0 20 0H60V40Z",
-  bottomLeft:
-    "M40 60C28.9543 60 20 51.0457 20 40C8.9543 40 0 31.0457 0 20C0 8.95431 8.9543 0 20 0H60V40C60 51.0457 51.0457 60 40 60Z",
+    "M124 40C124 51.0457 112.8071 60 99 60C94.5563 60 90.3837 59.0716 86.7676 57.4453L84.0713 60.1426L75.4639 51.5352L88 39L85.1719 36.1719L72.63574 48.707L64 40.0713L66.6416 37.4287C64.960359 33.7678 64 29.524 64 25C64 11.1929 72.95431 0 84 0H124V40Z",
   bottomRight:
-    "M60 20C60 31.0457 51.0457 40 40 40C40 51.0457 31.0457 60 20 60C8.9543 60 0 51.0457 0 40V0H40C51.0457 0 60 8.95431 60 20Z",
+    "M124 84C124 95.0457 115.0457 104 104 104C104 115.0457 95.0457 124 84 124C72.9543 124 64 115.0457 64 104V64H104C115.0457 64 124 72.95431 124 84Z",
+  bottomLeft:
+    "M40 124C28.9543 124 20 115.0457 20 104C8.9543 104 0 95.0457 0 84C0 72.95431 8.9543 64 20 64H60V104C60 115.0457 51.0457 124 40 124Z",
 } as const;
 
 type CloverLoadingMarkProps = {
@@ -22,26 +22,10 @@ export function CloverLoadingMark({ className }: CloverLoadingMarkProps) {
       xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
     >
-      <g>
-        <g className="clover-loading-screen__leaf clover-loading-screen__leaf--1">
-          <path d={markPaths.topLeft} fill="url(#clover-loading-screen-top-left)" />
-        </g>
-      </g>
-      <g transform="translate(64 0)">
-        <g className="clover-loading-screen__leaf clover-loading-screen__leaf--2">
-          <path d={markPaths.topRight} fill="url(#clover-loading-screen-top-right)" />
-        </g>
-      </g>
-      <g transform="translate(64 64)">
-        <g className="clover-loading-screen__leaf clover-loading-screen__leaf--3">
-          <path d={markPaths.bottomRight} fill="url(#clover-loading-screen-bottom-right)" />
-        </g>
-      </g>
-      <g transform="translate(0 64)">
-        <g className="clover-loading-screen__leaf clover-loading-screen__leaf--4">
-          <path d={markPaths.bottomLeft} fill="url(#clover-loading-screen-bottom-left)" />
-        </g>
-      </g>
+      <path className="clover-loading-screen__leaf clover-loading-screen__leaf--1" d={markPaths.topLeft} fill="url(#clover-loading-screen-top-left)" />
+      <path className="clover-loading-screen__leaf clover-loading-screen__leaf--2" d={markPaths.topRight} fill="url(#clover-loading-screen-top-right)" />
+      <path className="clover-loading-screen__leaf clover-loading-screen__leaf--3" d={markPaths.bottomRight} fill="url(#clover-loading-screen-bottom-right)" />
+      <path className="clover-loading-screen__leaf clover-loading-screen__leaf--4" d={markPaths.bottomLeft} fill="url(#clover-loading-screen-bottom-left)" />
       <defs>
         <linearGradient id="clover-loading-screen-top-left" x1="0" y1="0" x2="60" y2="60" gradientUnits="userSpaceOnUse">
           <stop offset="0.5" stopColor="#03A8C0" />
